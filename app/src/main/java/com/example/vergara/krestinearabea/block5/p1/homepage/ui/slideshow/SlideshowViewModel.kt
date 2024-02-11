366178405c16ad7de09a509031a0acb4d0fe14b8
HP <arabeavergara154@gmail.com>
2024-02-11 20:43:34 +0800

    1st Commit

---
+++ app/src/main/java/com/example/vergara/krestinearabea/block5/p1/homepage/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.vergara.krestinearabea.block5.p1.homepage.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
